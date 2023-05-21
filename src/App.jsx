58f1c03e22d920f1f43c1177f4edab3291d97960
AlexMunive <103223036+AlexMunive@users.noreply.github.com>
2023-05-21 12:21:10 -0500

    App-Todo

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import TodoComputed from "./components/TodoComputed";
+import TodoCreate from "./components/TodoCreate";
+import TodoFilter from "./components/TodoFilter";
+import TodoList from "./components/TodoList";
+
+// const initialState = [
+//     { id: 1, title: "Completed online javascript", completed: true },
+//     { id: 2, title: "Go to gym", completed: false },
+//     { id: 3, title: "10 min meditation", completed: true },
+//     { id: 4, title: "pick up groceries", completed: false },
+//     { id: 5, title: "completed app frontend mentor", completed: false },
+// ];
+
+const initialState = JSON.parse(localStorage.getItem("todos") || []);
+
+const App = () => {
+    const [todos, setTodos] = useState(initialState);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const createTodo = (title) => {
+        const newTodo = {
+            id: Date.now(),
+            title: title.trim(),
+            completed: true,
+        };
+
+        setTodos([...todos, newTodo]);
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    const updateTodo = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const computedItemsLeft = todos.filter((todo) => !todo.completed).length;
+
+    const clearCompleted = () => {
+        setTodos(todos.filter((todo) => !todo.completed));
+    };
+
+    const [filter, setFilter] = useState("all");
+
+    const changeFilter = (filter) => setFilter(filter);
+
+    const filterTodos = () => {
+        switch (filter) {
+            case "all":
+                return todos;
+            case "all":
+                return todos;
+            case "active":
+                return todos.filter((todo) => !todo.completed);
+            case "completed":
+                return todos.filter((todo) => todo.completed);
+            default:
+                return todos;
+        }
+    };
+
+    return (
+        <div className="dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]transition-all min-h-screen bg-gray-300 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain  bg-no-repeat duration-1000 dark:bg-gray-900 md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:md:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
+            <Header />
+            <main className="container mx-auto mt-8 px-4 md:max-w-xl">
+                <TodoCreate createTodo={createTodo} />
+
+                <TodoList
+                    todos={filterTodos()}
+                    removeTodo={removeTodo}
+                    updateTodo={updateTodo}
+                />
+
+                <TodoComputed
+                    computedItemsLeft={computedItemsLeft}
+                    clearCompleted={clearCompleted}
+                />
+
+                <TodoFilter changeFilter={changeFilter} filter={filter} />
+            </main>
+
+            <footer className="mt-8 text-center dark:text-gray-400">
+                Drag and drop to reorder list
+            </footer>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/TodoCreate.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const TodoCreate = ({ createTodo }) => {
+    const [title, setTitle] = useState("");
+
+    const handleSubmitAddTodo = (e) => {
+        e.preventDefault();
+
+        // validación
+
+        if (!title.trim()) {
+            return setTitle("");
+        }
+        createTodo(title);
+        setTitle("");
+    };
+
+    return (
+        <form
+            onSubmit={handleSubmitAddTodo}
+            className="mt-8 flex items-start gap-4 overflow-hidden rounded-md bg-white px-4 py-4 transition-all duration-1000 dark:bg-gray-800"
+        >
+            <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                type="text"
+                placeholder="Create a new todo..."
+                className="w-full text-gray-400 outline-none transition-all duration-1000 dark:bg-gray-800"
+            />
+        </form>
+    );
+};
+
+export default TodoCreate;
